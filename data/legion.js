0821b696784a6ae49547698598ef1e63c727f0e4
allknowingfrog <allknowingfrog@gmail.com>
2014-09-06 13:11:17 -0500

    first functional implementation

--- data/legion.js
+++ data/legion.js
@@ -1,2 +1,18 @@
-//$("body").css("border", "solid 5px red");
-document.body.style.border = "5px solid red";
+$(document).ready(function(){
+    var host = window.location.hostname;
+    var start = host.indexOf("www.");
+    if(start > -1) {
+        var end = host.indexOf(".", start + 4);
+        host = host.substring(start + 4, end);
+    } else {
+        var end = host.indexOf(".");
+        host = host.substring(0, end);
+    }
+         
+    //alert(host);
+    $("input[type='password']").each(function(){
+        var pass = hex_md5($(this).val() + host);
+        $(this).val(pass);
+        //alert(pass);
+    });
+});
